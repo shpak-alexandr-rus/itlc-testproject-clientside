0f302d9a3487833f2e38224d55b64d496c991dce
Alexandr Shpak <shpak.alexandr.sergeevich@yandex.ru>
2023-05-04 07:43:06 +0300

    [ClientSide-1] Initial commit

---
+++ src/main/java/ru/itlc/testproject/clientside/AppLauncher.java
@@ -0,0 +1,23 @@
+package ru.itlc.testproject.clientside;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class AppLauncher extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(AppLauncher.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
---
+++ src/main/java/ru/itlc/testproject/clientside/AppLauncherController.java
@@ -0,0 +1,14 @@
+package ru.itlc.testproject.clientside;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class AppLauncherController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
