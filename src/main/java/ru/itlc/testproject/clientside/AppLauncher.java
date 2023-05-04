590e6e6dd8dee49e4f208c637b5a45f795d3d634
Alexandr Shpak <shpak.alexandr.sergeevich@yandex.ru>
2023-05-04 10:29:39 +0300

    [ClientSide-5] Added alert if connection to server absent

--- src/main/java/ru/itlc/testproject/clientside/AppLauncher.java
+++ src/main/java/ru/itlc/testproject/clientside/AppLauncher.java
@@ -1,11 +1,13 @@
 package ru.itlc.testproject.clientside;
 
 import javafx.application.Application;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
 import ru.itlc.testproject.clientside.components.MainView;
 import ru.itlc.testproject.clientside.utils.HttpWorkUtils;
 
-import java.io.IOException;
+import java.util.Optional;
 
 public class AppLauncher extends Application {
 
@@ -18,12 +20,18 @@ public class AppLauncher extends Application {
 
     @Override
     public void start(Stage stage) {
-        new MainView();
-/*        FXMLLoader fxmlLoader = new FXMLLoader(AppLauncher.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();*/
+        if (connectionToRestStatus) {
+            new MainView();
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Сервер недоступен");
+            alert.setHeaderText("Не удалось соединиться с сервером.\nПроверьте его работу на localhost:8080");
+
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.isPresent() && result.get() == ButtonType.OK) {
+                System.exit(1);
+            }
+        }
     }
 
     public static void main(String[] args) {
