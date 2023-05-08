a8c2a9c8d23f64b828e64464a2b94a4f018da818
Alexandr Shpak <shpak.alexandr.sergeevich@yandex.ru>
2023-05-09 00:12:52 +0300

    [ClientSide-16] Added styles
    Added comments
    Deleted unnecessary files

--- src/main/java/ru/itlc/testproject/clientside/components/BookTableView.java
+++ src/main/java/ru/itlc/testproject/clientside/components/BookTableView.java
@@ -9,13 +9,19 @@ import javafx.util.converter.LongStringConverter;
 import ru.itlc.testproject.clientside.responses.Book;
 
 public class BookTableView extends VBox {
-	
+
 	private TableView<Book> table;
+	// Колонка таблицы для хранения ID
 	private TableColumn<Book, Long> bookId;
+	// Колонка таблицы для хранения автора книги
 	private TableColumn<Book, String> bookAuthor;
+	// Колонка таблицы для хранения названия книги
 	private TableColumn<Book, String> bookTitle;
+	// Колонка таблицы для хранения названия издательства
 	private TableColumn<Book, String> bookPublisher;
+	// Колонка таблицы для хранения адреса издательства
 	private TableColumn<Book, String> bookPublisherAddress;
+	// Колонка таблицы для хранения даты публикации книги
 	private TableColumn<Book, String> bookPublishingDate;
 
 	public BookTableView() {
@@ -23,55 +29,62 @@ public class BookTableView extends VBox {
 	}
 
 	private void buildUI() {
-		
+		// Создание таблицы
 		table = new TableView<>();
-//		table.setEditable(true);
 
+		// Создание колонки номера в каталоге
 		bookId = new TableColumn<>("Номер в каталоге");
 		bookId.setCellValueFactory(new PropertyValueFactory<>("bookId"));
 		bookId.setCellFactory(TextFieldTableCell.forTableColumn(new LongStringConverter()));
 
+		// Создание колонки автора книги
 		bookAuthor = new TableColumn<>("Авторы");
 		bookAuthor.setCellValueFactory(new PropertyValueFactory<>("bookAuthor"));
 		bookAuthor.setCellFactory(TextFieldTableCell.forTableColumn());
 
+		// Создание колонки названия книги
 		bookTitle = new TableColumn<>("Название");
 		bookTitle.setCellValueFactory(new PropertyValueFactory<>("bookTitle"));
 		bookTitle.setCellFactory(TextFieldTableCell.forTableColumn());
 
+		// Создание колонки названия издательства
 		bookPublisher = new TableColumn<>("Издательство");
 		bookPublisher.setCellValueFactory(new PropertyValueFactory<>("bookPublisher"));
 		bookPublisher.setCellFactory(TextFieldTableCell.forTableColumn());
 
+		// Создание колонки адреса издательства
 		bookPublisherAddress = new TableColumn<>("Адрес издательства");
 		bookPublisherAddress.setCellValueFactory(new PropertyValueFactory<>("bookPublisherAddress"));
 		bookPublisherAddress.setCellFactory(TextFieldTableCell.forTableColumn());
 
-		bookPublishingDate = new TableColumn<>("Название");
+		// Создание колонки даты публикации
+		bookPublishingDate = new TableColumn<>("Дата публикации");
 		bookPublishingDate.setCellValueFactory(new PropertyValueFactory<>("bookPublishingDate"));
 		bookPublishingDate.setCellFactory(TextFieldTableCell.forTableColumn());
 
+		// Добавление всех колонок в таблицу
 		table.getColumns().add(bookId);
 		table.getColumns().add(bookAuthor);
 		table.getColumns().add(bookTitle);
 		table.getColumns().add(bookPublisher);
 		table.getColumns().add(bookPublisherAddress);
 		table.getColumns().add(bookPublishingDate);
-
-		
 		table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
 		table.getItems().clear();
 		getChildren().add(table);
 	}
-	
+
+	// Добавить книгу в таблицу
 	public void add(Book book) {
 		table.getItems().add(book);
 	}
 
+	// Взять объект таблицы
 	public TableView<Book> getTableView() {
 		return table;
 	}
 
+	// Очистить содержимое таблицы
 	public void clear() {
 		table.getItems().clear();
 	}
